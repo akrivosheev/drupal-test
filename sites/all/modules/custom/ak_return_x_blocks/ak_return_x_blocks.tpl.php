71003707bc5e6027bd27c049fad8167b3efaf609
akrivosheev <alexkriver@yandex.ru>
2017-11-20 17:38:08 +0300

    debug 28 task

--- sites/all/modules/custom/ak_return_x_blocks/ak_return_x_blocks.tpl.php
+++ sites/all/modules/custom/ak_return_x_blocks/ak_return_x_blocks.tpl.php
@@ -8,6 +8,10 @@
 
 <div class="ak_return_x_blocks-template-wrapped">
     <div class="body">
+      <?php if (!$block): ?>
+          Блоков нет.
+      <?php else:?>
       <?php print $block;?>
+      <?php endif;?>
     </div>
 </div>
