511eac1ecb924aa38af0853b669c2ebe1e1f96da
akrivosheev <alexkriver@yandex.ru>
2017-11-24 17:00:39 +0300

    send form with ajax

--- sites/all/modules/custom/ak_contact/js/cleanForm.js
+++ sites/all/modules/custom/ak_contact/js/cleanForm.js
@@ -1,5 +1,5 @@
 (function($) {
     Drupal.ajax.prototype.commands.clean_form = function(ajax, response, status) {
-        jQuery(".form-class").trigger('reset');
+        jQuery(".ak-contact-username-email-form-class").trigger('reset');
     }
 }(jQuery));
