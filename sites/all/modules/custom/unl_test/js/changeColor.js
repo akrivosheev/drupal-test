0c113c5e40fbb2d9940329d99328aabeaf05cd0f
akrivosheev <alexkriver@yandex.ru>
2017-12-08 18:24:46 +0300

    debug

--- sites/all/modules/custom/unl_test/js/changeColor.js
+++ sites/all/modules/custom/unl_test/js/changeColor.js
@@ -1,22 +1,20 @@
 (function($) {
 
-Drupal.ajax.prototype.commands.color_links = function(ajax, response, status) {
-    var arr = jQuery(".link-list a");
-    var random_color = getRandomColor();
-    arr.css("color", random_color);
+    Drupal.ajax.prototype.commands.color_links = function(ajax, response, status) {
 
-    function getRandomColor() {
-        var letters = '0123456789ABCDEF';
-        var color = '#';
-        for (var i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
+        var arr = jQuery(".links.main-links a");
+        var color = response.color;
+        arr.css("color", color);
+        console.log(response.color);
+    };
+
+    Drupal.behaviors.unl_test = {
+        attach: function (context, settings) {
+            setInterval(function() {
+                var url = "/color_links";
+                var ajax = new Drupal.ajax(false, false, {url : url});
+                ajax.eventResponse(ajax, {});
+                }, 3000);
         }
-        return color;
-    }
-};
-
-setInterval(function() {
-    Drupal.ajax.prototype.commands.color_links()
-}, 3000);
-
+    };
 }(jQuery));
